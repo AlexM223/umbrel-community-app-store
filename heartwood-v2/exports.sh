9a9e266dac631b126c51224c23c6a98b90337c4f
Alex Martinez <142263262+AlexM223@users.noreply.github.com>
2026-07-10 10:56:41 -0500

    Add heartwood-v2 app package

---
+++ heartwood-v2/exports.sh
@@ -0,0 +1,2 @@
+export APP_HEARTWOOD_V2_MARIADB_PASSWORD="$(derive_entropy "${app_entropy_identifier}-mariadb-password")"
+export APP_HEARTWOOD_V2_MARIADB_ROOT_PASSWORD="$(derive_entropy "${app_entropy_identifier}-mariadb-root-password")"
